5e34bf17bf1def31be70e7184252515f0b91c1b2
Jan Alexander Steffens (heftig) <jan.steffens@gmail.com>
2020-01-27 18:11:05 +0100

    ZEN: INTERACTIVE: Use BFQ as the elevator for SQ devices

--- block/elevator.c
+++ block/elevator.c
@@ -568,7 +568,7 @@ static struct elevator_type *elevator_get_default(struct request_queue *q)
 	    !blk_mq_is_shared_tags(q->tag_set->flags))
 		return NULL;
 
-	return elevator_find_get("mq-deadline");
+	return elevator_find_get("bfq");
 }
 
 /*
