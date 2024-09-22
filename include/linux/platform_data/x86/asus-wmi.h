0322017a815ee231f2b7f051b43488970da78cec
Luke D. Jones <luke@ljones.dev>
2024-09-22 21:39:43 +1200

    platform/x86: asus-armoury: move existing tunings to asus-armoury module
    The fw_attributes_class provides a much cleaner interface to all of the
    attributes introduced to asus-wmi. This patch moves all of these extra
    attributes over to fw_attributes_class, and shifts the bulk of these
    definitions to a new kernel module to reduce the clutter of asus-wmi
    with the intention of deprecating the asus-wmi attributes in future.
    
    The work applies only to WMI methods which don't have a clearly defined
    place within the sysfs and as a result ended up lumped together in
    /sys/devices/platform/asus-nb-wmi/ with no standard API.
    
    Where possible the fw attrs now implement defaults, min, max, scalar,
    choices, etc. As en example dgpu_disable becomes:
    
    /sys/class/firmware-attributes/asus-armoury/attributes/dgpu_disable/
    ├── current_value
    ├── display_name
    ├── possible_values
    └── type
    
    as do other attributes.
    
    The ppt_* based attributes are removed in this initial patch as the
    implementation is somewhat broken due to the WMI methods requiring a
    set of limits on the values accepted (which is not provided by WMI).
    
    Signed-off-by: Luke D. Jones <luke@ljones.dev>

--- include/linux/platform_data/x86/asus-wmi.h
+++ include/linux/platform_data/x86/asus-wmi.h
@@ -143,6 +143,9 @@ struct asus_wmi {
 	struct asus_wmi_driver *driver;
 };
 
+#define ASUS_WMI_MGMT_GUID	"97845ED0-4E6D-11DE-8A39-0800200C9A66"
+#define ASUS_ACPI_UID_ASUSWMI	"ASUSWMI"
+
 /* WMI Methods */
 #define ASUS_WMI_METHODID_SPEC	        0x43455053 /* BIOS SPECification */
 #define ASUS_WMI_METHODID_SFBD		0x44424653 /* Set First Boot Device */
